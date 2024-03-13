7ec3b2384a81a8baea22ad552afbda33fda8fddc
Gabriel Elias <gabrielelias8@hotmail.com>
2024-03-13 20:21:06 -0300

    feat(Routes) Routes configurations

--- app/src/main/java/br/com/fiap/saudeplena/MainActivity.kt
+++ app/src/main/java/br/com/fiap/saudeplena/MainActivity.kt
@@ -3,31 +3,15 @@ package br.com.fiap.saudeplena
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import br.com.fiap.saudeplena.screens.LoginScreen
 import br.com.fiap.saudeplena.ui.theme.SaudePlenaTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,50 +21,17 @@ class MainActivity : ComponentActivity() {
             SaudePlenaTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = Color(0xFF070D1C),
+                    color = Color(0xFF070D1C)
                 ) {
-                    LoginScreen()
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = "login"
+                    ) {
+                        composable(route = "login") { LoginScreen(navController) }
+                    }
                 }
             }
         }
     }
 }
-
-
-@Composable
-fun LoginScreen() {
-    val image = painterResource(R.drawable.plus)
-    Column() {
-      Surface(
-          modifier = Modifier.fillMaxWidth()
-              .height(240.dp),
-          color = Color(0xFF222D48),
-          shape = RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp),
-      ){
-          Box(
-              modifier = Modifier.fillMaxSize(),
-              contentAlignment = Alignment.Center,
-          ){
-              Column(
-                  horizontalAlignment = Alignment.CenterHorizontally,
-                  verticalArrangement = Arrangement.spacedBy(0.dp),
-              ) {
-                  Image(
-                      painter = image,
-                      contentDescription = null,
-                      Modifier.height(40.dp)
-                          .width(40.dp)
-                  )
-
-                  Text("Login",
-                      color = Color(0xFFFFFFFF),
-                      fontSize = 40.sp,
-                      fontWeight = FontWeight.Bold,
-                      textAlign = TextAlign.Center,
-                  )
-              }
-            }
-
-          }
-      }
-    }
---
+++ app/src/main/java/br/com/fiap/saudeplena/screens/LoginScreen.kt
@@ -0,0 +1,129 @@
+package br.com.fiap.saudeplena.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.material3.TextFieldDefaults
+import androidx.navigation.NavController
+import br.com.fiap.saudeplena.ui.theme.SaudePlenaTheme
+
+@Composable
+fun LoginScreen (navController: NavController) {
+    //LoginTopHeader()
+    LoginBody()
+}
+
+
+/*
+@Composable
+fun LoginTopHeader() {
+    val image = painterResource(R.drawable.plus)
+    Column() {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(240.dp),
+            color = Color(0xFF222D48),
+            shape = RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp),
+        ){
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ){
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(0.dp),
+                ) {
+                    Image(
+                        painter = image,
+                        contentDescription = null,
+                        Modifier
+                            .height(40.dp)
+                            .width(40.dp)
+                    )
+
+                    Text("Login",
+                        color = Color(0xFFFFFFFF),
+                        fontSize = 40.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+            }
+
+        }
+    }
+}
+*/
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginBody() {
+
+    var textFieldValue = remember {
+        mutableStateOf("")
+    }
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ){
+        Column(
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+            ){
+                Text(
+                    text = "Email",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFFFFFFFF)
+                )
+
+                TextField(
+                    value = textFieldValue.value,
+                    onValueChange = { value ->
+                        textFieldValue.value = value
+                    },
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color(0xFF131A30)),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                    colors = TextFieldDefaults.textFieldColors(
+                        textColor = Color.White,
+                        cursorColor = Color.White,
+                        focusedIndicatorColor = Color(0xFF0E52C7),
+                        placeholderColor = Color.Gray,
+                    )
+                )
+            }
+        }
+    }
+}
